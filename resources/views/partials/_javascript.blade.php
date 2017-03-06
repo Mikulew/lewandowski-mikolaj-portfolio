223e713f953e33bf4c285c45194c6d90b7efba41
Mikulew <mikulew@gmail.com>
2017-03-06 18:03:35 +0100

    6th commit - responsive navbar

--- resources/views/partials/_javascript.blade.php
+++ resources/views/partials/_javascript.blade.php
@@ -1,13 +1,18 @@
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/semantic-ui/2.2.7/semantic.min.js"></script>
     <script>
-        $leftSidebar = $('.ui.sidebar.vertical.left.menu');
-        $rightSidebar = $('.ui.sidebar.vertical.right.menu');
-        $leftBtn = $('#toggle');
-        $rightBtn = $('#toggleAdmin');
-        
-            $leftSidebar.sidebar('attach events', $leftBtn);
-        
-            $rightSidebar.sidebar('attach events' ,$rightBtn);
+       var $leftSidebar = $('.ui.sidebar.vertical.left.menu');
+           var $leftBtn = $('#toggle');
+               $leftSidebar.sidebar('attach events', $leftBtn);
+
+           var $menu = $(".hamburger");
+           var $navbar = $('.navbar');
+
+           $menu.click('click', toggleMenu);
+
+           function toggleMenu() {
+               $menu.toggleClass('open');
+               $navbar.toggleClass('open');
+           }
     </script>
     <script src="/js/parsley.min.js"></script>
